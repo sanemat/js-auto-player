474fb9e52328201d7e899c2655db2bd0b334c823
sanemat <o.gata.ken@gmail.com>
2015-03-27 04:58:57 +0900

    chore(server): remove gulp-webserver

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,3 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-
-gulp.task('webserver', function() {
-  gulp.src('app')
-    .pipe($.webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true
-    }));
-});

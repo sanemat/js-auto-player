5e4c0ab62410e1107efa522b378d74e27851db2e
sanemat <o.gata.ken@gmail.com>
2015-03-30 01:16:20 +0900

    chore(react-youtube): use react-youtube

--- app/src/components/Player.jsx
+++ app/src/components/Player.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+
+var url = 'https://www.youtube.com/watch?v=MzLJxkOF6Sc';
 
 let Player = React.createClass({
+  getInitialState: function() {
+    return {
+      url: url
+    };
+  },
 
-  render() {
+  _onReady: function() {
+    console.log('READY');
+  },
+
+  _onPlay: function() {
+    console.log('PLAYING');
+  },
+
+  _onPause: function() {
+    console.log('PAUSED');
+  },
+
+  _onEnd: function() {
+    console.log('ENDED');
+  },
+
+  render: function() {
+    let playerOptions = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+
     return (
-      <div>
-        Player here.
-      </div>
+      <YouTube
+        url={this.state.url}
+        opts={playerOptions}
+        onReady={this._onReady}
+        onPlay={this._onPlay}
+        onPause={this._onPause}
+        onEnd={this._onEnd}
+      />
     );
   }
-
 });
 
 export default Player;
